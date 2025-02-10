885804d8fc176350d2ad0c2b9551b9d0ee6c3c82
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-02-10 11:33:42 +0100

    navmeny

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Button from '@mui/material/Button';
+import Navbar from './components/Navbar';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
  
 
   return (
     <>
+    <Navbar />
  <div style={{ padding: '2rem' }}>
       <h1>Hei, velkommen til Kennel Tonje!</h1>
       <Button variant="contained" color="primary">
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" className={styles.appBar}>
+      <Toolbar className={styles.toolbar}>
+        {/* Logo/Tittel */}
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          className={styles.logo}
+        >
+          Tonje Kennelen
+        </Typography>
+
+        {/* Desktop: Standard navigasjonsmeny */}
+        <Box className={styles.desktopNav}>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">Hjem</Link>
+            </li>
+            <li>
+              <Link to="/hunder">Våre Hunder</Link>
+            </li>
+            <li>
+              <Link to="/omoss">Om Oss</Link>
+            </li>
+            <li>
+              <Link to="/kontakt">Kontakt</Link>
+            </li>
+          </ul>
+        </Box>
+
+        {/* Mobil: Hamburger-ikon */}
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="toggle menu"
+          onClick={handleMenuClick}
+          className={styles.mobileMenuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* Mobil: Dropdown-meny */}
+        <Menu
+          id="nav-menu"
+          anchorEl={anchorEl}
+          open={isMenuOpen}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <MenuItem component={Link} to="/" onClick={handleClose}>
+            Hjem
+          </MenuItem>
+          <MenuItem component={Link} to="/hunder" onClick={handleClose}>
+            Våre Hunder
+          </MenuItem>
+          <MenuItem component={Link} to="/omoss" onClick={handleClose}>
+            Om Oss
+          </MenuItem>
+          <MenuItem component={Link} to="/kontakt" onClick={handleClose}>
+            Kontakt
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
