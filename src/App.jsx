46ab6a1668317ed828cbf4da3406a98629fb0f02
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-02-10 12:51:41 +0100

    fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import './index.css'
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
